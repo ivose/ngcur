67a8953d2ec5dc9c6df16fc468ec7e6f39bb5cad
Ivo <ivose@users.noreply.github.com>
2024-11-16 19:56:41 +0200

    section 4, l6pp

--- Sec4/src/app/user-input/user-input.component.ts
+++ Sec4/src/app/user-input/user-input.component.ts
@@ -1,11 +1,8 @@
 import { InvestmentService } from './../investment.service.';
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })

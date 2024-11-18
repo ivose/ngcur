edfde817752df86c08e2fcdbc733927311d641da
Ivo <ivose@users.noreply.github.com>
2024-11-18 15:10:56 +0000

    section 6

--- Sec6/src/app/dashboard/server-status/server-status.component.ts
+++ Sec6/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,10 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
-  constructor() {
+  constructor() {}
+  ngOnInit() {
     setInterval(() => {
       const rnd = Math.random();
       this.currentStatus = rnd > 0.5 ? 'online' : (rnd > 0.2 ? 'offline' : 'unknown');

6bab1839a05b8b06105552d30a465a9334de65dc
Ivo <ivose@users.noreply.github.com>
2024-11-26 07:48:58 +0000

    section 11

--- Sec11/src/app/app.component.ts
+++ Sec11/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, DestroyRef, OnInit, computed, effect, inject, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -7,7 +8,9 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
+  private message = signal('Hello World');
   clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
   interval = signal(0);
   doubleInterval = computed(() => this.interval() * 2);
   private destroyRef = inject(DestroyRef);
@@ -15,22 +18,24 @@ export class AppComponent implements OnInit {
     effect(() => {
       console.log(`Click count: ${this.clickCount()}`);
     });
+    toObservable(this.clickCount);
   }
   ngOnInit() {
-    setInterval(() => {
-      this.interval.update(i => i + 1);
+    // setInterval(() => {
+    //   this.interval.update(i => i + 1);
+    // });
+    console.log(this.message());
+    const subscription = interval(1000).pipe(
+      map((val) => val * 2)
+    ).subscribe({
+      next: (value) => console.log(value),
+      error: (err) => console.error(err),
+      complete: () => console.log('Complete')
     });
-    // const subscription = interval(1000).pipe(
-    //   map((val) => val * 2)
-    // ).subscribe({
-    //   next: (value) => console.log(value),
-    //   error: (err) => console.error(err),
-    //   complete: () => console.log('Complete')
-    // });
 
-    // this.destroyRef.onDestroy(() => {
-    //   subscription.unsubscribe();
-    // });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
   onClick() {

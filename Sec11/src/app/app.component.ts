fa4eb76cbf766b2ff447540329110fa71d62f6a7
Ivo <ivose@users.noreply.github.com>
2024-11-26 07:28:35 +0000

    section 11

--- Sec11/src/app/app.component.ts
+++ Sec11/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  ngOnInit() {
+    const subscription = interval(1000).subscribe({
+      next: (value) => console.log(value),
+      error: (err) => console.error(err),
+      complete: () => console.log('Complete')
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 
 }

a906af61272e78110ff45b8c5fa92e95900f17da
Ivo <ivose@users.noreply.github.com>
2024-11-30 13:47:02 +0200

    section 12

--- Sec12/src/app/places/available-places/available-places.component.ts
+++ Sec12/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, DestroyRef } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, subscribeOn, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,52 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  inFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  // constructor(private httpClient: HttpClient) {}
+  ngOnInit() {
+    this.inFetching.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places', {
+        //observe: 'event'
+      })
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.error(error);
+          return throwError(() => {
+            new Error("Couldn't fetch places")
+          })
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          //this.places.set(resData.places);//selle asemel pipe map
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.inFetching.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      console.log('Destroying AvailablePlacesComponent');
+      subscription.unsubscribe();
+    });
+  }
+
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient.put('http://localhost:3000/user-places', {
+      placeId: selectedPlace.id,
+    }).subscribe({
+      next: (resData) => console.log(resData)
+    });
+  }
 }
--- Sec12/src/app/places/user-places/user-places.component.ts
+++ Sec12/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,44 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  inFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.inFetching.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places', {
+        //observe: 'event'
+      })
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.error(error);
+          return throwError(() => {
+            new Error("Something went wrong fetching your favourite places. Please try again later.")
+          })
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          //this.places.set(resData.places);//selle asemel pipe map
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.inFetching.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      console.log('Destroying AvailablePlacesComponent');
+      subscription.unsubscribe();
+    });
+  }
 }

3b04cfae1731a035d90d14e4c695e15d996cd814
Ivo <ivose@users.noreply.github.com>
2024-11-30 22:13:38 +0200

    section 13, template driven form

--- Sec13/src/app/auth/login/login.component.ts
+++ Sec13/src/app/auth/login/login.component.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { debounceTime, subscribeOn } from 'rxjs';
+import { afterNextRender, Component, viewChild, DestroyRef, inject } from '@angular/core';
+import { EmailValidator, FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('saved-login-form');
+      if (savedForm) {
+        console.log({savedForm});
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail);
+        }, 1);
+      }
+      const subscription = this.form().valueChanges?.pipe(debounceTime(1000)).subscribe({
+        next: (value) => {
+          console.log(value);
+          window.localStorage.setItem(
+            'saved-login-form', 
+            JSON.stringify({
+              email: value.email,
+              password: value.password
+            }));
+        },
+      });
+      this.destroyRef.onDestroy(() => {
+        subscription?.unsubscribe();
+      });
+    });
+  }
+
+
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      return;
+    }
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+    console.log(enteredEmail, enteredPassword);
+    console.log(formData.form);
+    formData.form.reset();
+  }
+
+}

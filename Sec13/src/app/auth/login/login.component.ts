71f7f4002e9e8d552beb60dd3dfca6638bafbb89
Ivo <ivose@users.noreply.github.com>
2024-12-01 10:23:25 +0200

    section 13, reactive form

--- Sec13/src/app/auth/login/login.component.ts
+++ Sec13/src/app/auth/login/login.component.ts
@@ -1,9 +1,72 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { of } from 'rxjs';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+  return { doesNotContainQuestionMark: true };
+}
+
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@example.com') {
+    return of(null);
+  }
+  return of({ notUnique: true });
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark],
+    }),
+  });
+
+  get emailErrors() {
+    // return (this.form.controls.email.touched
+    //   && this.form.controls.email.dirty
+    //   && this.form.controls.email.invalid)
+    const control = this.form.controls.email;
+    if (control.touched && control.dirty && control.errors) {
+      if (control.errors['required']) {
+        return 'Email is required';
+      }
+      if (control.errors['email']) {
+        return 'Please enter a valid email address';
+      }
+    }
+    return null;
+  }
+  get passwordErrors() {
+    const control = this.form.controls.password;
+    if (control.touched && control.dirty && control.errors) {
+      if (control.errors['required']) {
+        return 'Password is required';
+      }
+      if (control.errors['minlength']) {
+        return 'Password must be at least 6 characters long';
+      }
+    }
+    return null;
+  }
+
+  onSubmit() {
+    console.log(this.form);
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail, enteredPassword);
+
+  }
+}

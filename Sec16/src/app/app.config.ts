45ff8e9ea74ade4721c70312fd4e6a80ad3d288c
Ivo <ivose@users.noreply.github.com>
2024-12-04 18:58:08 +0200

    section 16

--- Sec16/src/app/app.config.ts
+++ Sec16/src/app/app.config.ts
@@ -8,6 +8,7 @@ import {
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +18,6 @@ export const appConfig: ApplicationConfig = {
       withRouterConfig({
         paramsInheritanceStrategy: 'always',
       })
-    ), provideFirebaseApp(() => initializeApp({"projectId":"ng-deployment-example-9fb07","appId":"1:489026928294:web:0295b3459ae8507c1fbe67","storageBucket":"ng-deployment-example-9fb07.firebasestorage.app","apiKey":"AIzaSyAii-HRx57FjYPE1WyjOSq3L84ywidV5NM","authDomain":"ng-deployment-example-9fb07.firebaseapp.com","messagingSenderId":"489026928294"})), provideFirestore(() => getFirestore()),
+    ), provideFirebaseApp(() => initializeApp({"projectId":"ng-deployment-example-9fb07","appId":"1:489026928294:web:0295b3459ae8507c1fbe67","storageBucket":"ng-deployment-example-9fb07.firebasestorage.app","apiKey":"AIzaSyAii-HRx57FjYPE1WyjOSq3L84ywidV5NM","authDomain":"ng-deployment-example-9fb07.firebaseapp.com","messagingSenderId":"489026928294"})), provideFirestore(() => getFirestore()), provideClientHydration(),
   ],
 };

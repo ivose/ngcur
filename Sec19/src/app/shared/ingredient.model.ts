acca225a6a7aa69f7034fcf0abc26214899ee377
Ivo <ivose@users.noreply.github.com>
2024-12-08 12:09:15 +0200

    section 19

---
+++ Sec19/src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+    constructor(public name: string, public amount: number) {}
+    // või 
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+}

f9d19ed914be8d5c8e9a1614d5471d9edb77ea79
Ivo <ivose@users.noreply.github.com>
2025-01-26 20:11:59 +0200

    section 29

---
+++ Sec29/src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstSubscription: Subscription;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.firstSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firstSubscription.unsubscribe();//ilma selleta uuesti sellele urlile tulles oleks ngOnInit'is topelt protsessid
+  }
+
+}

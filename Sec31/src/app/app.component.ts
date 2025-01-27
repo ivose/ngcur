85da44f01be891133152014a9ae45d7a9121091d
Ivo <ivose@users.noreply.github.com>
2025-01-27 19:50:08 +0200

    section 31

--- Sec31/src/app/app.component.ts
+++ Sec31/src/app/app.component.ts
@@ -11,17 +11,30 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders=['male','female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    this.signupForm.setValue({
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: "",
+    //   gender: 'male',
+    // });
+    this.signupForm.form.patchValue({
       userData: {
-        username: suggestedName,
-        email: '',
-      },
-      secret: 'pet',
-      questionAnswer: "",
-      gender: 'male',
+        userName: suggestedName,
+      }
     });
   }
 
@@ -30,5 +43,13 @@ export class AppComponent {
   // }
   onSubmit() {
     console.log('submitted', this.signupForm)
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.userData.defaultQuestion;
+    this.user.answer = this.signupForm.value.userData.questonAnswer;
+    this.user.gender = this.signupForm.value.userData.gender;
+
+    this.signupForm.reset();
   }
 }

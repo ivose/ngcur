15a088203c7040d8b9ea2f88e3fa117348890a75
Ivo <ivose@users.noreply.github.com>
2025-01-27 19:31:26 +0200

    section 31

--- Sec31/src/app/app.component.ts
+++ Sec31/src/app/app.component.ts
@@ -10,9 +10,19 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders=['male','female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: '',
+      },
+      secret: 'pet',
+      questionAnswer: "",
+      gender: 'male',
+    });
   }
 
   // onSubmit(form: NgForm) {

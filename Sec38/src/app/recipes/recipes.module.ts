c96a68ad3475cac01d9519d019bef74a2fa5b439
Ivo <ivose@users.noreply.github.com>
2025-02-02 20:16:02 +0200

    section 38

--- Sec38/src/app/recipes/recipes.module.ts
+++ Sec38/src/app/recipes/recipes.module.ts
@@ -9,6 +9,8 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -19,14 +21,12 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     RecipeStartComponent,
     RecipeEditComponent
   ],
-  imports: [RouterModule, CommonModule, ReactiveFormsModule],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
+  imports: [
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RecipesRoutingModule,
+    SharedModule,
   ]
 })
-export class RecipesModule {}
+export class RecipesModule { }
---
+++ Sec38/src/app/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { PlaceholderDirective } from './placeholder/placeholder.directive';
+import { NgModule } from "@angular/core";
+import { AlertComponent } from "./alert/alert.component";
+import { LoadingSpinnerComponent } from "./loading-spinner/loading-spinner.component";
+import { DropdownDirective } from './dropdown.directive';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations:  [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropdownDirective,
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropdownDirective,
+    CommonModule
+  ],
+  entryComponents: [
+    AlertComponent,
+  ]
+})
+export class SharedModule {
+
+}

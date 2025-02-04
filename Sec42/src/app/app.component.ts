e402ba2f48ee39f12252bad70027eadce2f2c990
Ivo <ivose@users.noreply.github.com>
2025-02-04 19:38:40 +0200

    section 42

--- Sec42/src/app/app.component.ts
+++ Sec42/src/app/app.component.ts
@@ -1,13 +1,37 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal => hightlighted', animate(300)),
+      transition('hightlighted => normal', animate(500)),
+    ])
+  ]
 })
 export class AppComponent {
+  state = "normal";
   list = ['Milk', 'Sugar', 'Bread'];
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAnimate(item) {
+    this.state = this.state == 'normal' ? 'highlighted' : 'normal';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1)
+  }
 }
